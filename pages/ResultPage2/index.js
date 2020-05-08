5665429aca3c2395f51dc23556d239d0a50cfd91
= <=>
2020-05-08 14:52:14 -0700

    fix bugs

--- pages/ResultPage2/index.js
+++ pages/ResultPage2/index.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import Header from '../../comps/Header';
-// import Button from '../../comps/Button';
-// import ResultTitle from '../../comps/ResultTitle';
-// import ResultImage2 from '../../comps/ResultImage2';
-// import ResultExplanation2 from '../../comps/ResultExplanation2';
-// import '../HomePage/page1.css';
+import React from 'react';
+import Header from '../../comps/Header';
+import Button from '../../comps/Button';
+import ResultTitle from '../../comps/ResultTitle';
+import ResultImage1 from '../../comps/ResultImage1';
+import ResultExplanation1 from '../../comps/ResultExplanation1';
+import '../HomePage/page1.css';
 
-// const picToShow = require('../../comps/ResultImage/face2.png')
+const picToShow = require('../../comps/ResultImage/face2.png')
 
-// const ResultPage2 = ({profpic}) =>
-//     <div id="app">
-//         <Header />
-//         <ResultTitle />
-//         <ResultImage2 
-//         profpic={picToShow}/>
-//         <div class="resultbox second">
-//         <ResultExplanation2
-//             text="Wow, you are on the right track but there's still room for improvement. We have some tips to help you out! "/>
-//         </div>
+const ResultPage2 = ({profpic}) =>
+    <div id="app">
+        <Header />
+        <ResultTitle />
+        <ResultImage1 
+        profpic={picToShow}/>
+        <div class="resultbox second">
+        <ResultExplanation1
+            text="Wow, you are on the right track but there's still room for improvement. We have some tips to help you out! "/>
+        </div>
     
-//             <Button
-//                 width="190px"
-//                 text="Check Tips" />
+            <Button
+                width="190px"
+                text="Check Tips" />
         
-//     </div>
+    </div>
 
-// export default ResultPage2;
+export default ResultPage2;
--- pages/ResultPage3/index.js
+++ pages/ResultPage3/index.js
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import Header from '../../comps/Header';
-// import Button from '../../comps/Button';
-// import ResultTitle from '../../comps/ResultTitle';
-// import ResultImage3 from '../../comps/ResultImage3';
-// import ResultExplanation3 from '../../comps/ResultExplanation3';
-// import '../HomePage/page1.css';
+import React from 'react';
+import Header from '../../comps/Header';
+import Button from '../../comps/Button';
+import ResultTitle from '../../comps/ResultTitle';
+import ResultImage1 from '../../comps/ResultImage1';
+import ResultExplanation1 from '../../comps/ResultExplanation1';
+import '../HomePage/page1.css';
 
-// const picToShow = require('../../comps/ResultImage/face3.png')
+const picToShow = require('../../comps/ResultImage/face3.png')
 
-// const ResultPage3 = ({ profpic }) =>
-//     <div id="app">
-//         <Header />
-//         <ResultTitle />
-//         <ResultImage3
-//             profpic={picToShow} />
-//         <div class="resultbox third">
-//             <ResultExplanation3
-//                 text="You are a sustainable superstar! Looks like you already know the benefits of a sustainable lifestyle. 
-//             We have some tips on how you can best spread your knowledge to those around you!"/>
-//         </div>
-//             <Button
-//                 width="190px"
-//                 text="Check Tips" />
-//     </div>
+const ResultPage3 = ({ profpic }) =>
+    <div id="app">
+        <Header />
+        <ResultTitle />
+        <ResultImage1
+            profpic={picToShow} />
+        <div class="resultbox third">
+            <ResultExplanation1
+                text="You are a sustainable superstar! Looks like you already know the benefits of a sustainable lifestyle. 
+            We have some tips on how you can best spread your knowledge to those around you!"/>
+        </div>
+            <Button
+                width="190px"
+                text="Check Tips" />
+    </div>
 
-// export default ResultPage3;
+export default ResultPage3;

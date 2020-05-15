c7da19d9b1d4be6838cee7fb67228e64e278be99
= <=>
2020-05-14 22:33:57 -0700

    fix bugs

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5e86e71adbf712dd590c";
+/******/ 	var hotCurrentHash = "54d977d6b43891a8fa15";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
